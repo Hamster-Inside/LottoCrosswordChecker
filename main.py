22f52a8c33bed1dcb17121f44132bea78c98949a
Arek <abudkowski@interia.pl>
2022-07-08 06:57:21 +0200

    stage 1

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 letters = input("Write your letters\n")
-words = input("Write your words")
+words_puzzle = {}
+words_puzzle[0] = input("Write your words\n")
+words_puzzle[1] = input("Write your words\n")
+words_puzzle[2] = input("Write your words\n")
+
+for i in words_puzzle:
+    words = []
+    words = words_puzzle[i].split(" ")
+    print(words)
+
